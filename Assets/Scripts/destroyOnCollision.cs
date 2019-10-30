dc96738cee6a5a8fe0c44d52a8e636b1191a4e58
Linetes <A01207499@itesm.mx>
2019-10-30 15:18:09 -0600

    collisions Destroy

---
+++ Assets/Scripts/destroyOnCollision.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class destroyOnCollision : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void OnCollisionEnter(Collision collision)
+    {
+        Destroy(collision.collider.gameObject);
+        Destroy(gameObject);
+    }
+}
---
+++ Assets/Scripts/powerUpSpawn.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class powerUpSpawn : MonoBehaviour
+{
+    public int powerUpsNum;
+    public int minx, maxx, minz, maxz;
+    public GameObject spawn;
+    private Vector3 pos = new Vector3();
+    // Start is called before the first frame update
+    void Start()
+    {
+        for (int i = 0; i < powerUpsNum; i++)
+        {
+            pos = new Vector3(Random.Range(minx, maxx), 0, Random.Range(minz, maxz));
+            Instantiate(spawn, pos, transform.rotation);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
